ecedd5a13eb6d72a5af14d749446cb7f79a9948e
Qiu Wenbo <qiuwenbo@phytium.com.cn>
2020-03-10 09:21:08 +0800

    loader: disable mod elf on ARM64 to pass the test coverage
    Signed-off-by: Qiu Wenbo <qiuwenbo@phytium.com.cn>

--- src/loader/mod.rs
+++ src/loader/mod.rs
@@ -49,6 +49,7 @@ pub mod start_info;
 #[allow(non_snake_case)]
 #[allow(non_upper_case_globals)]
 #[cfg_attr(feature = "cargo-clippy", allow(clippy::all))]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 mod elf;
 mod struct_util;
 
